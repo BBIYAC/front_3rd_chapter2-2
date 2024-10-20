3191c775d4828b1426c06ac6bd246ebeb055f98f
김인영 <kiy7833@gmail.com>
2024-10-21 01:38:32 +0900

    :recycle: Entity 컴포넌트와 UI 컴포넌트 분리 및 Context API 사용

---
+++ src/refactoring/contexts/useCartContext.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext } from "react";
+import { useCart } from "../hooks";
+import { ICartItem, ICoupon, IProduct } from "../../types";
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+interface ICartContextType {
+  cart: ICartItem[];
+  addToCart: (product: IProduct) => void;
+  removeFromCart: (productId: string) => void;
+  updateQuantity: (productId: string, newQuantity: number) => void;
+  applyCoupon: (coupon: ICoupon) => void;
+  calculateTotal: () => any;
+  selectedCoupon: ICoupon | null;
+}
+
+const CartContext = createContext<ICartContextType | undefined>(undefined);
+
+export const CartProvider = ({ children }: IProps) => {
+  return (
+    <CartContext.Provider value={useCart()}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCartContext = () => {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw new Error('useCartContext must be used within a CartProvider');
+  }
+
+  return context;
+};
